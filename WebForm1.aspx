7e3667a196772b9c7bc0c5fd776e059cfff6dff0
AliUgras <48187387+AliUgras@users.noreply.github.com>
2022-09-30 19:54:36 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LibraryPenaltyCase.WebForm1" EnableEventValidation="false" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <div class="jumbotron">
+        <h1>Kütüphane Sistemine Hoşgeldiniz</h1>
+        <h2>Lütfen kitabınızı aldığınız tarihi seçiniz.</h2>
+            <asp:Calendar ID="clAlimTarihi" runat="server" BackColor="White" BorderColor="#999999" CellPadding="4" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="Black" Height="180px" Width="200px">
+                <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" />
+                <NextPrevStyle VerticalAlign="Bottom" />
+                <OtherMonthDayStyle ForeColor="#808080" />
+                <SelectedDayStyle BackColor="#666666" Font-Bold="True" ForeColor="White" />
+                <SelectorStyle BackColor="#CCCCCC" />
+                <TitleStyle BackColor="#999999" BorderColor="Black" Font-Bold="True" />
+                <TodayDayStyle BackColor="#CCCCCC" ForeColor="Black" />
+                <WeekendDayStyle BackColor="#FFFFCC" />
+            </asp:Calendar>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Ulkeler]"></asp:SqlDataSource>
+    </div>
+    <div class="jumbotron">
+        <h2>Lütfen kitabınızı teslim ettiğiniz tarihi seçiniz</h2>
+                <asp:Calendar ID="clTeslimTarihi" runat="server" BackColor="White" BorderColor="#999999" CellPadding="4" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="Black" Height="180px" Width="200px">
+                    <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" />
+                    <NextPrevStyle VerticalAlign="Bottom" />
+                    <OtherMonthDayStyle ForeColor="#808080" />
+                    <SelectedDayStyle BackColor="#666666" Font-Bold="True" ForeColor="White" />
+                    <SelectorStyle BackColor="#CCCCCC" />
+                    <TitleStyle BackColor="#999999" BorderColor="Black" Font-Bold="True" />
+                    <TodayDayStyle BackColor="#CCCCCC" ForeColor="Black" />
+                    <WeekendDayStyle BackColor="#FFFFCC" />
+                </asp:Calendar>
+        
+    </div>
+    <div class="jumbotron">
+        <h2>Lütfen ülkenizi seçiniz   
+            <asp:DropDownList ID="ddlUlkeler" runat="server" DataSourceID="SqlDataSource1" DataTextField="UlkeAdi" DataValueField="UlkeAdi">
+            </asp:DropDownList>
+        </h2>
+    </div>
+    <div class="jumbotron">
+        <asp:Button ID="btCezaHesapla" runat="server" Text="Ceza Hesapla" Height="45px" Width="124px" OnClick="btCezaHesapla_Click1" />
+    </div>
+    <div class="jumbotron">
+         <h1>Sonuç</h1>
+        <h2>Hesaplanan Gün Sayısı : 
+
+            <asp:Label ID="lbGunSayisi" runat="server"></asp:Label>
+
+        </h2>
+        <h2>Hesaplanan Ceza Miktarı :
+
+            <asp:Label ID="lbCezaMiktari" runat="server"></asp:Label>
+
+        </h2>
+    </div>
+</asp:Content>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using LibraryPenaltyCase.Classlar;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibraryPenaltyCase
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        #region Değişkenler
+
+        SqlConnection baglanti = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        public static float cezaMiktari;
+        public static string paraBirimi;
+
+        #endregion
+        #region Constructor
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        #endregion
+        protected void btCezaHesapla_Click1(object sender, EventArgs e)
+        {
+            var tatilGunleri = TatilGunleriGetir();
+            var cezaGunleri = new List<DateTime>();
+
+            for (var i = clAlimTarihi.SelectedDate; i <= clTeslimTarihi.SelectedDate; i = i.AddDays(1))  //seçilen başlangıç ve bitiş tarihleri arasını for ile dön -- daha sonra bir kitap classı oluşturulup orada bir fonksiyon olarak konulabilir
+            {
+                if (i.DayOfWeek == DayOfWeek.Sunday || i.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    //cumartesi ve ya pazar günlerini ceza listesine ekleme
+                }
+                else if (tatilGunleri.Any(x => x.TatilGunu.Day == i.Day && x.TatilGunu.Month == i.Month))
+                {
+                    //Resmi tatilleri ceza listesine ekleme
+                }
+                else
+                    cezaGunleri.Add(i);  //içerisinde kitap alınmasından iade edilmesine kadar tatil olmayan her günü barındıran list buradan ilk 10 günü çıkar kalan günü cezalandır.
+            }
+
+            if (cezaGunleri.Count > 10)
+            {
+                cezaGunleri.RemoveRange(0, 10);
+                lbGunSayisi.Text = cezaGunleri.Count().ToString();
+                lbCezaMiktari.Text = (cezaGunleri.Count() * cezaMiktari).ToString();
+                // var cezaUcreti = cezaGunleri.Count()* //ülke tablosundan ceza miktarını ve para birimini çekip ekrana yaz
+            }
+            else
+            {
+                //ceza kesilmeyeceği zaman yapılacak işler
+            }
+        }
+        public List<TatilGunleri> TatilGunleriGetir()
+        {
+            List<TatilGunleri> tatilGunleri = new List<TatilGunleri>();
+            baglanti.Open();
+
+            string sorgu = "select * from TatilGunleri where UlkeAdi = '" + ddlUlkeler.SelectedValue + "'"; //Sadece seçilen ülkeye ait tatil günlerini getir
+
+            SqlCommand komut = new SqlCommand(sorgu, baglanti);
+
+            var dataReader = komut.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                var tatilGunu = new TatilGunleri()
+                {
+                    TatilGunu = (DateTime)dataReader["Tarih"],
+                    UlkeAdi = dataReader["UlkeAdi"].ToString()
+                };
+
+                tatilGunleri.Add(tatilGunu);   //tatil günlerinin tamamını datareader'dan listeye al kullanım kolaylığı için
+            }
+            dataReader.Close();
+
+            sorgu= "Select * from Ulkeler where UlkeAdi ='" + ddlUlkeler.SelectedValue + "'";
+
+            komut = new SqlCommand(sorgu, baglanti);
+
+            dataReader = komut.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                paraBirimi = dataReader["ParaBirimi"].ToString();
+                cezaMiktari = float.Parse(dataReader["CezaMiktari"].ToString());
+            }
+            dataReader.Close();
+            baglanti.Close();
+            return tatilGunleri;
+        }
+    }
+}
